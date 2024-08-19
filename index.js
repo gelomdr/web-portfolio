513998b3171a88cd515b422969bed3c7eb09e028
gelomdr <amadorgelo@gmail.com>
2024-08-19 14:02:08 +0800

    Add option for dark mode

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleSwitch = document.getElementById('theme-toggle');
+    const themeCaption = document.getElementById('theme-caption');
+    const body = document.body;
+
+    // Check if a theme is saved in localStorage
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+        toggleSwitch.checked = true;
+    }
+
+    toggleSwitch.addEventListener('change', function() {
+        if (toggleSwitch.checked) {
+            body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark'); // Save theme preference to local storage
+        } else {
+            body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light'); // Save theme preference to local storage
+        }
+    });
+});
